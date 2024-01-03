164f5dbda3a4de2792d6f8e015364b62952d9885
BeyzanurYuce <beyza@hammurabigames.com>
2024-01-03 15:27:33 +0300

    Initial commit

---
+++ engine.py
@@ -0,0 +1,223 @@
+import torch
+import math
+import os
+import time
+import copy
+import numpy as np
+from tqdm import tqdm
+from lib.logger import get_logger
+from lib.metrics import All_Metrics
+from tensorboardX import SummaryWriter
+import torch.nn.functional as F
+
+class Engine(object):
+    def __init__(self, model, loss, optimizer, train_loader, val_loader, test_loader,
+                 scaler, args, lr_scheduler=None):
+        self.current_epoch = 0 
+        super(Engine, self).__init__()
+        self.model = model
+        self.loss = loss
+        self.optimizer = optimizer
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.test_loader = test_loader
+        self.scaler = scaler
+        self.args = args
+        self.lr_scheduler = lr_scheduler
+        self.train_per_epoch = len(train_loader)
+        if val_loader != None:
+            self.val_per_epoch = len(val_loader)
+        self.best_path = os.path.join(self.args.log_dir, '{}_{}_best_model.pth'.format(args.dataset, args.model))
+        self.loss_figure_path = os.path.join(self.args.log_dir, 'loss.png')
+        # log
+        if os.path.isdir(args.log_dir) == False and not args.debug:
+            os.makedirs(args.log_dir, exist_ok=True)  # run.log
+        self.logger = get_logger(args.log_dir, name=args.model, debug=args.debug)
+        self.logger.info('Experiment log path in: {}'.format(args.log_dir))
+         # Create a SummaryWriter for TensorBoard
+        self.writer = SummaryWriter(log_dir="/logs_gauss")
+        
+
+    
+    def train_epoch(self):
+        self.model.train()
+        total_loss = 0
+        total_mae = 0
+        total_rmse = 0
+        total_mape = 0
+        for batch_idx, (data, target) in enumerate(self.train_loader):
+            data = data[..., :1]
+            label = target[..., :1].to(data.device)    # (..., 1)
+            # data and target shape: B, T, N, F; output shape: B, T, N, F
+            self.optimizer.zero_grad()
+            output = self.model(data)
+            if self.args.real_value:
+                label = self.scaler.inverse_transform(label)
+            output = output.to(label.device)
+            loss = self.loss(output, label)
+            mae = torch.abs(output - label).mean()
+            rmse = torch.sqrt(F.mse_loss(output, label))
+            mape = (torch.abs(output - label) / (label + 1e-7)).mean() * 100
+            total_mae += mae.item()
+            total_rmse += rmse.item()
+            total_mape += mape.item()
+            loss.backward()
+
+            # add max grad clipping
+            if self.args.grad_norm:
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.args.max_grad_norm)
+            self.optimizer.step()
+            total_loss += loss.item()
+        
+        train_epoch_loss = total_loss / self.train_per_epoch
+        train_epoch_mae = total_mae / self.train_per_epoch
+        train_epoch_rmse = total_rmse / self.train_per_epoch
+        train_epoch_mape = total_mape / self.train_per_epoch
+
+        # learning rate decay
+        if self.args.lr_decay:
+            self.lr_scheduler.step()
+       
+        return train_epoch_loss,train_epoch_mae, train_epoch_rmse, train_epoch_mape
+
+
+    def val_epoch(self, val_dataloader):
+        self.model.eval()
+        total_val_loss = 0
+        total_val_mae = 0
+        total_val_rmse = 0
+        total_val_mape = 0
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(val_dataloader):
+                data = data[..., :1]
+                label = target[..., :1].to(data.device) 
+                output = self.model(data)
+                output = output.to(label.device)
+                if self.args.real_value:
+                    label = self.scaler.inverse_transform(label)
+                loss = self.loss(output, label)
+                mae = torch.abs(output - label).mean()
+                rmse = torch.sqrt(F.mse_loss(output, label))
+                mape = (torch.abs(output - label) / (label + 1e-7)).mean() * 100
+
+                if not torch.isnan(loss):
+                    total_val_loss += loss.item()
+                total_val_mae += mae.item()
+                total_val_rmse += rmse.item()
+                total_val_mape += mape.item()
+        num_batches = len(val_dataloader)
+        val_loss = total_val_loss / num_batches
+        val_mae = total_val_mae / num_batches
+        val_rmse = total_val_rmse / num_batches
+        val_mape = total_val_mape / num_batches
+
+        return val_loss, val_mae, val_rmse, val_mape
+
+
+    def train(self):
+        best_model = None
+        best_loss = float('inf')
+        not_improved_count = 0
+        train_loss_list = []
+        val_loss_list = []
+        start_time = time.time()
+        for epoch in tqdm(range(1, self.args.epochs + 1)):
+            self.current_epoch = epoch
+            t1 = time.time()
+            train_epoch_loss, train_epoch_mae, train_epoch_rmse, train_epoch_mape = self.train_epoch()
+            self.writer.add_scalar('Loss/Train', train_epoch_loss, epoch)
+            self.writer.add_scalar('Metrics/MAE_Train', train_epoch_mae, epoch)
+            self.writer.add_scalar('Metrics/RMSE_Train', train_epoch_rmse, epoch)
+            self.writer.add_scalar('Metrics/MAPE_Train', train_epoch_mape, epoch)
+            t2 = time.time()
+            if self.val_loader == None:
+                val_dataloader = self.test_loader
+            else:
+                val_dataloader = self.val_loader
+            t3 = time.time()
+            val_epoch_loss, val_epoch_mae, val_epoch_rmse, val_epoch_mape= self.val_epoch(val_dataloader)
+            self.writer.add_scalar('Loss/Validation', val_epoch_loss, epoch)
+            self.writer.add_scalar('Metrics/MAE_Val', val_epoch_mae, epoch)
+            self.writer.add_scalar('Metrics/RMSE_Val', val_epoch_rmse, epoch)
+            self.writer.add_scalar('Metrics/MAPE_Val', val_epoch_mape, epoch)
+            t4 = time.time()
+            self.logger.info('Epoch {:03d}, Train Loss: {:.4f}, Valid Loss: {:.4f}, Training Time: {:.4f} secs.'.format(epoch, train_epoch_loss, val_epoch_loss, (t2 - t1)))
+            print("Inference Time: {:.4f} secs.", (t4 - t3))
+            train_loss_list.append(train_epoch_loss)
+            val_loss_list.append(val_epoch_loss)
+            if train_epoch_loss > 1e6:
+                self.logger.warning('Gradient explosion detected. Ending...')
+                break
+            if val_epoch_loss < best_loss:
+                best_loss = val_epoch_loss
+                not_improved_count = 0
+                best_state = True
+            else:
+                not_improved_count += 1
+                best_state = False
+            # early stop
+            if self.args.early_stop:
+                if not_improved_count == self.args.early_stop_patience:
+                    self.logger.info("Validation performance didn\'t improve for {} epochs. "
+                                    "Training stops.".format(self.args.early_stop_patience))
+                    break
+            # save the best state
+            if best_state == True:
+                self.logger.info('Current best model saved!')
+                best_model = copy.deepcopy(self.model.state_dict())
+                torch.save(best_model, self.best_path)
+
+        training_time = time.time() - start_time
+        self.logger.info("Total training time: {:.4f} min, best loss: {:.6f}".format((training_time / 60), best_loss))
+
+        # save the best model to file
+        self.logger.info("Saving current best model to " + self.best_path)
+
+        # test
+        self.model.load_state_dict(best_model)
+        self.test(self.model, self.args, self.test_loader, self.scaler, self.logger)
+
+
+    def save_checkpoint(self):
+        state = {
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'config': self.args
+        }
+        torch.save(state, self.best_path)
+        self.logger.info("Saving current best model to " + self.best_path)
+
+
+    @staticmethod
+    def test(model, args, data_loader, scaler, logger, path=None):
+        if path != None:
+            check_point = torch.load(path)
+            state_dict = check_point['state_dict']
+            args = check_point['config']
+            model.load_state_dict(state_dict)
+            model.to(args.device)
+        model.eval()
+        y_pred = []
+        y_true = []
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(data_loader):
+                data = data[..., :1].to(args.device)
+                label = target[..., :1].to(args.device)
+                output = model(data)
+                y_true.append(label)
+                y_pred.append(output)
+        y_true = scaler.inverse_transform(torch.cat(y_true, dim=0)).to(args.device)
+        if args.real_value:
+            y_pred = torch.cat(y_pred, dim=0).to(args.device).to(args.device)
+        else:
+            y_pred = scaler.inverse_transform(torch.cat(y_pred, dim=0)).to(args.device)
+        print(y_true.cpu().numpy().shape)
+        # np.save('./NewNet_{}_true.npy'.format(args.dataset), y_true.cpu().numpy())
+        # np.save('./NewNet_{}_pred.npy'.format(args.dataset), y_pred.cpu().numpy())
+
+        for t in range(y_true.shape[1]):
+            mae, rmse, mape = All_Metrics(y_pred[:, t, ...], y_true[:, t, ...], args.mae_thresh, args.rmse_thresh, args.mape_thresh)
+            logger.info("Horizon {:02d}, MAE: {:.2f}, RMSE: {:.2f}, MAPE: {:.4f}%".format(t + 1, mae, rmse, mape * 100))
+            # Store metric values in dictionaries
+        mae, rmse, mape = All_Metrics(y_pred, y_true, args.mae_thresh, args.rmse_thresh, args.mape_thresh)
+        logger.info("Average Horizon, MAE: {:.2f}, RMSE: {:.2f}, MAPE: {:.4f}%".format(mae, rmse, mape * 100))
---
+++ train.py
@@ -0,0 +1,288 @@
+import os
+import torch
+import numpy as np
+import torch.nn as nn
+from config import args
+from datetime import datetime
+import torch.nn.functional as F
+from model.AFDGCN import Model as Network
+from engine import Engine
+from lib.metrics import MAE_torch
+from lib.TrainInits import init_seed
+from lib.dataloader import get_dataloader
+from lib.TrainInits import print_model_parameters
+from lib.load_graph import get_Gaussian_matrix, get_adjacency_matrix
+import scipy.sparse as sp
+from torch.utils.tensorboard import SummaryWriter
+
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+# *****************************************  参数初始化配置      Parametre başlatma yapılandırması ****************************************** #
+init_seed(args.seed)
+'''
+if torch.cuda.is_available():
+    torch.cuda.set_device(int(args.device[0]))
+else:
+'''
+args.device = 'cpu'
+
+#A = get_Gaussian_matrix(args.graph_path, args.num_nodes, args.normalized_k, id_filename=args.filename_id)
+
+def myminimum(A,B):
+    BisBigger = A-B
+    BisBigger.data = np.where(BisBigger.data >= 0, 1, 0)
+    return A - A.multiply(BisBigger) + B.multiply(BisBigger)
+
+def normalize_adjHPI(adj):
+    adj = sp.coo_matrix(adj)
+
+    rowsum = np.array(adj.sum(1))
+    
+    deg_row = np.tile(rowsum, (1,adj.shape[0]))
+    
+    #deg_row = deg_row.T
+    deg_row = sp.coo_matrix(deg_row)
+    
+    sim = adj.dot(adj)
+    
+    #y = sim.copy().tocsr()
+    #y.data.fill(1)
+    X = sim.astype(bool).astype(int)
+    deg_row = deg_row.multiply(X)
+    
+    deg_row = myminimum(deg_row, deg_row.T)
+    
+    sim = sim/deg_row
+    #sim = sp.coo_matrix(sim)
+    whereAreNan = np.isnan(sim)
+    whereAreInf = np.isinf(sim)
+    sim[whereAreNan] = 0
+    sim[whereAreInf] = 0
+    
+    sim = sp.coo_matrix(sim)
+    #print(sim[0])
+    return sim.toarray()
+
+def normalize_adjAA(W):
+    # Calculate row sums
+    rowsum = np.array(W.sum(1))
+    # Check for zero values in rowsum
+    zero_indices = np.where(rowsum == 0)
+    # Handle zero values by setting them to a small positive number (e.g., 1e-6)
+    rowsum[zero_indices] = 1e-6
+    # Calculate the inverse root of rowsum
+    d_inv_sqrt = np.power(np.log(rowsum), -1).flatten()
+    # Handle infinity values by setting them to 0
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.0
+    # Create a diagonal matrix from d_inv_sqrt
+    d_mat_inv_sqrt = np.diag(d_inv_sqrt)
+    # Calculate the normalized adjacency matrix
+    DA = d_mat_inv_sqrt.dot(W)
+    
+    return W.dot(DA)
+
+
+def normalize_adjCN(W):
+   # ctx = mx.gpu()
+    rowsum = np.array(W.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+   # d_mat_inv_sqrt = mx.nd.array(np.diag(d_inv_sqrt), ctx=ctx)
+    return np.dot(W, W)
+
+
+def normalize_adjHDI(adj):
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1))
+    deg_row = np.tile(rowsum, (1, adj.shape[0]))
+    deg_row = sp.coo_matrix(deg_row)
+
+    sim = adj.dot(adj)
+
+    X = sim.astype(bool).astype(int)
+    deg_row = deg_row.multiply(X)
+
+    # Handle division by zero by checking for zero entries in deg_row
+    nonzero_indices = deg_row.data.nonzero()  # Get indices where data is non-zero
+    deg_row.data[nonzero_indices] = 1.0 / deg_row.data[nonzero_indices]
+    deg_row.data[~np.isfinite(deg_row.data)] = 0  # Set inf and NaN values to zero
+
+    sim = sim.multiply(deg_row)
+
+    sim = mymaximum(sim, sim.T)  # Ensure mymaximum is defined
+
+    sim = np.array(sim.toarray())  # Convert the result to a NumPy array
+
+    return sim
+
+
+def mymaximum (A, B):
+    BisBigger = A-B
+    BisBigger.data = np.where(BisBigger.data <= 0, 1, 0)
+    return A - A.multiply(BisBigger) + B.multiply(BisBigger)
+import numpy as np
+
+def normalize_adjRA(adj):
+    """Symmetrically normalize adjacency matrix."""
+    rowsum = np.sum(adj, axis=1)
+    d_inv_sqrt = np.power(rowsum, -1 / 2)
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    D = np.diag(d_inv_sqrt)
+    DA = np.dot(D, adj)
+    return np.dot(adj, DA)
+
+import numpy as np
+
+def normalize_rvr(adj):
+    """Normalize the adjacency matrix using RVR."""
+
+    # Compute the row-normalized Laplacian matrix.
+    rowsum = np.sum(adj, axis=1)
+    L = adj / rowsum
+
+    # Compute the RVR matrix.
+    R = np.linalg.inv(L)
+
+    # Normalize the RVR matrix.
+    R_norm = R / np.linalg.norm(R, 'fro')
+
+    return R_norm
+
+
+import numpy as np
+
+
+def normalize_adj_RVR(adj):
+    """Perform Row-wise Vector Renormalization (RVR) on the adjacency matrix."""
+    # Compute the row sums
+    rowsum = np.sum(adj, axis=1)
+
+    # Compute the inverse square root of the row sums
+    d_inv_sqrt = 1.0 / np.sqrt(rowsum)
+
+    # Replace any inf or nan values with 0 to avoid division by zero
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0
+    d_inv_sqrt[np.isnan(d_inv_sqrt)] = 0
+
+    # Create a diagonal matrix with the inverse square root of the row sums
+    d_mat_inv_sqrt = np.diag(d_inv_sqrt)
+
+    # Perform RVR normalization
+    normalized_adj = np.dot(np.dot(d_mat_inv_sqrt,adj),d_mat_inv_sqrt)
+
+    return normalized_adj
+
+
+# load dataset
+#Adj = get_adjacency_matrix(args.graph_path, args.num_nodes, type='connectivity', id_filename=args.filename_id)
+Adj = get_Gaussian_matrix(args.graph_path, args.num_nodes, args.normalized_k, id_filename=args.filename_id)
+A=normalize_adjCN(Adj)
+A = torch.FloatTensor(Adj).to(args.device)
+train_loader, val_loader, test_loader, scaler = get_dataloader(args,
+                                                               normalizer=args.normalizer,
+                                                               tod=args.tod, 
+                                                               dow=False,
+                                                               weather=False, 
+                                                               single=False)
+print("train loader ",len(train_loader))
+# *****************************************  初始化模型参数 ****************************************** #
+input_dim = 1
+hidden_dim = 64 
+output_dim = 1
+embed_dim = 8   
+cheb_k = 2
+horizon = 12 
+num_layers = 1 
+heads = 4
+timesteps = 12
+kernel_size = 5
+model = Network(num_node = args.num_nodes, 
+                input_dim = input_dim, 
+                hidden_dim = hidden_dim, 
+                output_dim = output_dim, 
+                embed_dim = embed_dim, 
+                cheb_k = cheb_k, 
+                horizon = horizon, 
+                num_layers = num_layers, 
+                heads = heads, 
+                timesteps = timesteps, 
+                A = A,
+                kernel_size=kernel_size)
+model = model.to(args.device)
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1, 2, 3, 4'
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# if torch.cuda.device_count() > 1:
+#     model = nn.DataParallel(model.cuda(), device_ids=[3, 4])
+for p in model.parameters():
+    if p.dim() > 1:
+        nn.init.xavier_uniform_(p)
+    else:
+        nn.init.uniform_(p)
+# print the number of model parameters
+print_model_parameters(model, only_num=False)
+# *****************************************  定义损失函数、优化器 ****************************************** #
+# 1. init loss function, optimizer
+def masked_mae_loss(scaler, mask_value):
+    def loss(preds, labels):
+        if scaler:
+            preds = scaler.inverse_transform(preds)
+            labels = scaler.inverse_transform(labels)
+        mae = MAE_torch(pred=preds, true=labels, mask_value=mask_value)
+        return mae
+    return loss
+if args.loss_func == 'mask_mae':
+    loss = masked_mae_loss(scaler, mask_value=0.0)
+elif args.loss_func == 'mae':
+    loss = torch.nn.L1Loss().to(args.device)
+elif args.loss_func == 'mse':
+    loss = torch.nn.MSELoss().to(args.device)
+elif args.loss_func == 'smoothloss':
+    loss = torch.nn.SmoothL1Loss().to(args.device)
+else:
+    raise ValueError
+# 2. init optimizer
+optimizer = torch.optim.Adam(params=model.parameters(), lr=args.lr_init, eps=1.0e-8,
+                             weight_decay=0, amsgrad=False)
+# 3. learning rate decay
+lr_scheduler = None
+if args.lr_decay:
+    print('Applying learning rate decay.')
+    lr_decay_steps = [int(i) for i in list(args.lr_decay_step.split(','))]
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer,
+                                                        milestones=lr_decay_steps,
+                                                        gamma=args.lr_decay_rate)
+# *****************************************  模型训练与测试 ****************************************** #
+# 1.config log path
+current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+current_dir = os.path.dirname(os.path.realpath(__file__))
+log_dir = os.path.join(current_dir,'experiments', args.dataset, current_time)
+os.makedirs(log_dir, exist_ok=True)
+print(log_dir)
+args.log_dir = log_dir
+
+# 2.start training
+trainer = Engine(model, 
+                  loss, 
+                  optimizer, 
+                  train_loader, 
+                  val_loader, 
+                  test_loader, 
+                  scaler,
+                  args, 
+                  lr_scheduler)
+if args.mode == 'train':
+    trainer.train()
+elif args.mode == 'test':
+    checkpoint = "./experiments/PEMS04/20220811215513/Modified_PEMS04_AFDGCN_best_model.pth"
+    model.load_state_dict(torch.load(checkpoint, map_location=torch.device('cpu')))
+
+   # model.load_state_dict(torch.load(checkpoint))  # map_location='cuda:5'
+    # node_embedding = model.node_embedding
+    # adj = F.softmax(F.relu(torch.mm(node_embedding, node_embedding.transpose(0, 1))), dim=1)
+    # adj = torch.mm(node_embedding, node_embedding.transpose(0, 1))
+    # print(adj.shape)
+    # np.save('adaptive_matrix.npy', adj.detach().cpu().numpy())
+    print("load saved model...")
+    trainer.test(model, trainer.args, test_loader, scaler, trainer.logger)
+else:
+    raise ValueError
