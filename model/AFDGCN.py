f9e3a4233e3435b51c9715f127f95dbe62e33378
BeyzanurYuce <94442443+BeyzanurYuce@users.noreply.github.com>
2024-05-03 13:57:06 +0300

    Update AFDGCN.py with BernNet

--- model/AFDGCN.py
+++ model/AFDGCN.py
@@ -6,38 +6,44 @@ from torch.autograd import Variable
 from torch_geometric.nn import MessagePassing
 from torch_geometric.nn import GCNConv
 from torch.nn import Linear, Parameter
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
+import pandas as pd
+
 
 # Global Attention Mechanism
 class feature_attention(nn.Module):
     def __init__(self, input_dim, output_dim, kernel_size=5, rate=4):
         super(feature_attention, self).__init__()
         self.nconv = nn.Conv2d(input_dim, output_dim, kernel_size=(1, 1))
-        self.channel_attention = nn.Sequential(  
-            nn.Linear(output_dim, int(output_dim / rate)),  
-            nn.ReLU(inplace=True),  
-            nn.Linear(int(output_dim / rate), output_dim)  
+        self.channel_attention = nn.Sequential(
+            nn.Linear(output_dim, int(output_dim / rate)),
+            nn.ReLU(inplace=True),
+            nn.Linear(int(output_dim / rate), output_dim)
         )
-        self.spatial_attention = nn.Sequential(  
-            nn.Conv2d(output_dim, int(output_dim / rate), kernel_size=(1, kernel_size), padding=(0, (kernel_size - 1) // 2)),  
-            nn.BatchNorm2d(int(output_dim / rate)),  
-            nn.ReLU(inplace=True),  
-            nn.Conv2d(int(output_dim / rate), output_dim, kernel_size=(1, kernel_size), padding=(0, (kernel_size - 1) // 2)),  
-            nn.BatchNorm2d(output_dim)  
+        self.spatial_attention = nn.Sequential(
+            nn.Conv2d(output_dim, int(output_dim / rate), kernel_size=(1, kernel_size),
+                      padding=(0, (kernel_size - 1) // 2)),
+            nn.BatchNorm2d(int(output_dim / rate)),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(int(output_dim / rate), output_dim, kernel_size=(1, kernel_size),
+                      padding=(0, (kernel_size - 1) // 2)),
+            nn.BatchNorm2d(output_dim)
         )
-        
+
     def forward(self, x):
         x = x.permute(0, 3, 2, 1)  # [B, D, N, T]
         x = self.nconv(x)  # 扩展数据的特征维度
-        b, c, n, t = x.shape  
+        b, c, n, t = x.shape
         x_permute = x.permute(0, 2, 3, 1)  # [B, N, T, C]
-        x_att_permute = self.channel_attention(x_permute) 
+        x_att_permute = self.channel_attention(x_permute)
         x_channel_att = x_att_permute.permute(0, 3, 1, 2)  # [B, C, N, T]
         x = x * x_channel_att
         x_spatial_att = self.spatial_attention(x).sigmoid()
         out = x * x_spatial_att
         return out.permute(0, 3, 2, 1)
 
-class AVWGCN(nn.Module): #hid=64 + 64, 2 * 64, 2, 8
+
+class AVWGCN(nn.Module):  # hid=64 + 64, 2 * 64, 2, 8
     def __init__(self, in_dim, out_dim, cheb_k, embed_dim):
         """
         :param in_dim: 输入维度
@@ -46,11 +52,11 @@ class AVWGCN(nn.Module): #hid=64 + 64, 2 * 64, 2, 8
         :param embed_dim: 节点的嵌入维度
         """
         super(AVWGCN, self).__init__()
-        self.cheb_k = cheb_k                                            #embed_dim, cheb_k, in_dim, out_dim
-        self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, in_dim, out_dim))#8,2,128,128
-        self.bias_pool = nn.Parameter(torch.FloatTensor( embed_dim, out_dim))
-    
-    def forward(self, x, adj):
+        self.cheb_k = cheb_k  # embed_dim, cheb_k, in_dim, out_dim
+        self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, in_dim, out_dim))  # 8,2,128,128
+        self.bias_pool = nn.Parameter(torch.FloatTensor(embed_dim, out_dim))
+
+    def forward_adj(self, x, adj):
         """
         :param x: (B, N, C_in)
         :param node_embedding: (N, D), 这里的node_embedding是可学习的
@@ -59,17 +65,17 @@ class AVWGCN(nn.Module): #hid=64 + 64, 2 * 64, 2, 8
         node_num = adj.shape[0]
         # 自适应的学习节点间的内在隐藏关联获取邻接矩阵
         # D^(-1/2)AD^(-1/2)=softmax(ReLU(E * E^T)) - (N, N)
-        #support = F.softmax(F.relu(torch.mm(adj, adj.transpose(0, 1))), dim=1)
-        support=F.softmax(F.relu(adj.t()), dim=1)
-        #support = node_embedding
+        # support = F.softmax(F.relu(torch.mm(adj, adj.transpose(0, 1))), dim=1)
+        support = F.softmax(F.relu(adj.t()), dim=1)
+        # support = node_embedding
 
         # 这里得到的support表示标准化的拉普拉斯矩阵
         support_set = [torch.eye(node_num).to(support.device), support]
         for k in range(2, self.cheb_k):
             # Z(k) = 2 * L * Z(k-1) - Z(k-2)
             support_set.append(torch.matmul(2 * support, support_set[-1]) - support_set[-2])
-            #support_set.append(support_set[-1])
-        supports = torch.stack(support_set, dim=0) # (K, N, N)
+            # support_set.append(support_set[-1])
+        supports = torch.stack(support_set, dim=0)  # (K, N, N)
         # (N, D) * (D, K, C_in, C_out) -> (N, K, C_in, C_out)
         weights = torch.einsum('nd, dkio->nkio', adj, self.weights_pool)
         # (N, D) * (D, C_out) -> (N, C_out)
@@ -81,8 +87,7 @@ class AVWGCN(nn.Module): #hid=64 + 64, 2 * 64, 2, 8
         x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias  # (B, N, C_out)
         return x_gconv
 
-
-    def forward2(self, x, node_embedding):
+    def forward(self, x, node_embedding):
         """
         :param x: (B, N, C_in)
         :param node_embedding: (N, D), 这里的node_embedding是可学习的
@@ -92,15 +97,15 @@ class AVWGCN(nn.Module): #hid=64 + 64, 2 * 64, 2, 8
         # 自适应的学习节点间的内在隐藏关联获取邻接矩阵
         # D^(-1/2)AD^(-1/2)=softmax(ReLU(E * E^T)) - (N, N)
         support = F.softmax(F.relu(torch.mm(node_embedding, node_embedding.transpose(0, 1))), dim=1)
-        #support = node_embedding
+        # support = node_embedding
 
         # 这里得到的support表示标准化的拉普拉斯矩阵
         support_set = [torch.eye(node_num).to(support.device), support]
         for k in range(2, self.cheb_k):
             # Z(k) = 2 * L * Z(k-1) - Z(k-2)
             support_set.append(torch.matmul(2 * support, support_set[-1]) - support_set[-2])
-            #support_set.append(support_set[-1])
-        supports = torch.stack(support_set, dim=0) # (K, N, N)
+            # support_set.append(support_set[-1])
+        supports = torch.stack(support_set, dim=0)  # (K, N, N)
         # (N, D) * (D, K, C_in, C_out) -> (N, K, C_in, C_out)
         weights = torch.einsum('nd, dkio->nkio', node_embedding, self.weights_pool)
         # (N, D) * (D, C_out) -> (N, C_out)
@@ -111,6 +116,7 @@ class AVWGCN(nn.Module): #hid=64 + 64, 2 * 64, 2, 8
         x_g = x_g.permute(0, 2, 1, 3)  # (B, N, K, C_in) * (N, K, C_in, C_out)
         x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias  # (B, N, C_out)
         return x_gconv
+
 
 class AGCRNCell(nn.Module):
     def __init__(self, num_node, in_dim, out_dim, cheb_k, embed_dim):
@@ -127,16 +133,16 @@ class AGCRNCell(nn.Module):
         # 两个门控 forget、update
         z_r = torch.sigmoid(self.gate(input_and_state, node_embedding))
         z, r = torch.split(z_r, self.hidden_dim, dim=-1)
-        candidate = torch.cat((x, r*state), dim=-1)
+        candidate = torch.cat((x, r * state), dim=-1)
         hc = torch.tanh(self.update(candidate, node_embedding))
         h = z * state + (1 - z) * hc
         return h
 
     def init_hidden_state(self, batch_size):
         return torch.zeros(batch_size, self.num_node, self.hidden_dim)
-    
 
-class AVWDCRNN(nn.Module):  #AVWDCRNN(num_node, hidden_dim, hidden_dim, cheb_k, embed_dim, num_layers)
+
+class AVWDCRNN(nn.Module):  # AVWDCRNN(num_node, hidden_dim, hidden_dim, cheb_k, embed_dim, num_layers)
     def __init__(self, num_node, in_dim, out_dim, cheb_k, embed_dim, num_layers=1):
         super(AVWDCRNN, self).__init__()
         assert num_layers >= 1, "At least one DCRNN layer in the Encoder."
@@ -165,7 +171,7 @@ class AVWDCRNN(nn.Module):  #AVWDCRNN(num_node, hidden_dim, hidden_dim, cheb_k, 
                 state = self.dcrnnn_cells[i](current_inputs[:, t, :, :], state, node_embedding)
                 inner_states.append(state)
             output_hidden.append(state)  # 最后一个时间步输出的隐藏状态
-            current_inputs = torch.stack(inner_states, dim=1) # (B, T, N, hid_dim)
+            current_inputs = torch.stack(inner_states, dim=1)  # (B, T, N, hid_dim)
 
         # current_inputs: the outputs of last layer: (B, T, N, hidden_dim)
         # output_hidden: the last state for each layer: (num_layers, B, N, hidden_dim)
@@ -177,16 +183,17 @@ class AVWDCRNN(nn.Module):  #AVWDCRNN(num_node, hidden_dim, hidden_dim, cheb_k, 
         for i in range(self.num_layers):
             init_states.append(self.dcrnnn_cells[i].init_hidden_state(batch_size))
         return torch.stack(init_states, dim=0)  # (num_layers, B, N, hidden_dim)
-    
+
     def init_hidden2(self, batch_size, adj):
         init_states = []  # Initialize hidden states list for all layers
         for i in range(self.num_layers):
             # Assuming each cell in dcrnnn_cells has an init_hidden_state method
             init_states.append(self.dcrnnn_cells[i].init_hidden_state(batch_size, adj))
-        
+
         # Stack the initialized states along the first dimension to get (num_layers, B, N, hidden_dim)
-        return torch.stack(init_states, dim=0) 
-    
+        return torch.stack(init_states, dim=0)
+
+
 class PositionalEncoding(nn.Module):
     def __init__(self, out_dim, max_len=12):
         super(PositionalEncoding, self).__init__()
@@ -206,6 +213,7 @@ class PositionalEncoding(nn.Module):
         x = x + Variable(self.pe.to(x.device), requires_grad=False)
         return x
 
+
 class MultiHeadAttention(nn.Module):
     def __init__(self, embed_size, heads):
         super(MultiHeadAttention, self).__init__()
@@ -215,7 +223,7 @@ class MultiHeadAttention(nn.Module):
         self.heads = heads
         # 要求嵌入层特征维度可以被heads整除
         assert embed_size % heads == 0
-        self.head_dim = embed_size // heads   # every head dimension
+        self.head_dim = embed_size // heads  # every head dimension
 
         self.W_V = nn.Linear(self.embed_size, self.head_dim * heads, bias=False)
         self.W_K = nn.Linear(self.embed_size, self.head_dim * heads, bias=False)
@@ -235,7 +243,7 @@ class MultiHeadAttention(nn.Module):
         :param x: [B, T, N, D]
         """
         batch_size, _, _, d_k = x.shape
-        x = self.positional_encoding(x).permute(0, 2, 1, 3)   # [B, N, T, D]
+        x = self.positional_encoding(x).permute(0, 2, 1, 3)  # [B, N, T, D]
         # 计算Attention的Q、K、V
         Q = self.W_Q(x)
         K = self.W_K(x)
@@ -246,13 +254,14 @@ class MultiHeadAttention(nn.Module):
         # 考虑上下文的长期依赖信息
         scores = torch.matmul(Q, K.transpose(-1, -2)) / torch.sqrt(torch.tensor(d_k, dtype=torch.float))
         attention = F.softmax(scores, dim=-1)  # [k * B, N, T, T]
-        context = torch.matmul(attention, V)   # context vector
+        context = torch.matmul(attention, V)  # context vector
         context = torch.cat(torch.split(context, batch_size, dim=0), dim=-1)
-        context = context + x    # residual connection
+        context = context + x  # residual connection
         out = self.norm1(context)
         out = self.fc(out) + context  # residual connection
         out = self.norm2(out)
         return out
+
 
 class GraphAttentionLayer(nn.Module):
     def __init__(self, in_features, out_features, adj, dropout, alpha, concat=True):
@@ -315,34 +324,26 @@ class GPR_prop(MessagePassing):
         elif Init == 'WS':
             TEMP = Gamma
 
-        self.temp = Parameter(TEMP)
+        self.temp = Parameter(torch.tensor(TEMP))
 
     def reset_parameters(self):
-        if self.Init == 'SGC':
-            self.temp.data = torch.zeros_like(self.temp.data)
-            self.temp.data[int(self.alpha)] = 1.0
-        elif self.Init == 'PPR':
-            for k in range(self.K + 1):
-                self.temp.data[k] = self.alpha * (1 - self.alpha) ** k
-            self.temp.data[-1] = (1 - self.alpha) ** self.K
-        elif self.Init == 'NPPR':
-            for k in range(self.K + 1):
-                self.temp.data[k] = self.alpha ** k
-            self.temp.data /= torch.sum(torch.abs(self.temp.data))
-        elif self.Init == 'Random':
-            bound = torch.sqrt(torch.tensor(3.0 / (self.K + 1)))
-            self.temp.data = torch.rand_like(self.temp.data) * 2 * bound - bound
-            self.temp.data /= torch.sum(torch.abs(self.temp.data))
-        elif self.Init == 'WS':
-            self.temp.data = self.Gamma
+        torch.nn.init.zeros_(self.temp)
+        for k in range(self.K + 1):
+            self.temp.data[k] = self.alpha * (1 - self.alpha) ** k
+        self.temp.data[-1] = (1 - self.alpha) ** self.K
 
     def forward(self, x, edge_index):
-        edge_index, norm = GCNConv.norm(edge_index, num_nodes=x.size(0), dtype=x.dtype)
-
+        edge_index, norm = gcn_norm(edge_index, num_nodes=x.size(1), dtype=x.dtype)
+        # edge_index, norm = custom_gcn_norm(edge_index, num_nodes=x.size(1), dtype=x.dtype)
         hidden = x * self.temp[0]
+        x = x.T
+        hidden = hidden.T
         for k in range(self.K):
             x = self.propagate(edge_index, x=x, norm=norm)
+            # x = self.custom_propagate(edge_index, x=x, norm=norm)
+            # x = self.custom_propagate(edge_index, x=x.T, norm=norm)
             gamma = self.temp[k + 1]
+            # hidden = hidden + gamma * x
             hidden = hidden + gamma * x
         return hidden
 
@@ -354,31 +355,52 @@ class GPR_prop(MessagePassing):
 
 
 class GPRGNN(torch.nn.Module):
-    def __init__(self,input_dim,output_dim, hidden,cheb_k,num_layers):
+    def __init__(self, num_node, input_dim, output_dim, hidden, cheb_k, num_layers, embed_dim):
         super(GPRGNN, self).__init__()
-        self.lin1 = Linear(input_dim, hidden)
-        self.lin2 = Linear(hidden, output_dim)
+        self.lin1 = Linear(1216, 1)  # (input_dim, hidden) 19, 1
+        self.lin2 = Linear(1, 1216)
 
         self.prop1 = GPR_prop(cheb_k, 0.5, 'PPR', None)
 
         self.dprate = 0.5
         self.dropout = 0.2
-        self.num_layers=num_layers
+        self.num_layers = num_layers
+        ###
+        self.dcrnnn_cells = nn.ModuleList()
+        self.dcrnnn_cells.append(AGCRNCell(num_node, input_dim, output_dim, cheb_k, embed_dim))
+        for _ in range(1, num_layers):
+            self.dcrnnn_cells.append(AGCRNCell(num_node, input_dim, output_dim, cheb_k, embed_dim))
 
     def reset_parameters(self):
         self.prop1.reset_parameters()
 
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-
+    def forward(self, x):
+        edge_index = read_edge_list_csv()
         x = F.dropout(x, p=self.dropout, training=self.training)
-        x = F.relu(self.lin1(x))
+        x = x.to('cpu')
+        x_reshaped = x.reshape(x.size(0), -1)  # -1 infers the remaining dimension based on the input shape
+
+        # Apply linear layer
+        x = F.relu(self.lin1(x_reshaped))
+        # x = F.relu(self.lin1(x))
         x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.lin2(x)
 
-        x = self.prop1(x, edge_index)
-        return F.log_softmax(x, dim=1)
-    
+        if self.dprate == 0.0:
+            x = self.prop1(x, edge_index)
+            return F.log_softmax(x, dim=1)
+        else:
+            x = F.dropout(x, p=self.dprate, training=self.training)
+            x = self.prop1(x, edge_index)
+            x = x.transpose(0, 1)
+
+            # Reshape it from (5, 1216) to (5, 1, 19, 64)
+            x = x.view(x.size(0), 1, 19, 64)  # Manually reshape to (5, 1, 19, 64)
+
+            # Apply log softmax along the appropriate dimension
+            x = F.log_softmax(x, dim=3)  # Assuming the last dimension (64) is the one to apply softmax to
+            return x
+
     def init_hidden(self, batch_size):
         """
         Initialize hidden states for all layers.
@@ -393,13 +415,28 @@ class GPRGNN(torch.nn.Module):
         for i in range(self.num_layers):
             # Assuming each cell in dcrnnn_cells has an init_hidden_state method
             init_states.append(self.dcrnnn_cells[i].init_hidden_state(batch_size))
-        
+
         # Stack the initialized states along the first dimension to get (num_layers, B, N, hidden_dim)
         return torch.stack(init_states, dim=0)
 
 
+def read_edge_list_csv():
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv('/content/AFDGCN_BerNet/data/PEMS04/conn_graph.csv')
+
+    # Extract the 'from' and 'to' columns as numpy arrays
+    edges_from = df['from'].to_numpy()
+    edges_to = df['to'].to_numpy()
+
+    # Create the edge index tensor
+    edge_index = torch.tensor([edges_from, edges_to], dtype=torch.long)
+
+    return edge_index
+
+
 class Model(nn.Module):
-    def __init__(self, num_node, input_dim, hidden_dim, output_dim, embed_dim, cheb_k, horizon, num_layers, heads, timesteps, A, kernel_size):
+    def __init__(self, num_node, input_dim, hidden_dim, output_dim, embed_dim, cheb_k, horizon, num_layers, heads,
+               timesteps, A, kernel_size):
         super(Model, self).__init__()
         self.A = A
         self.timesteps = timesteps
@@ -414,8 +451,8 @@ class Model(nn.Module):
         self.node_embedding = nn.Parameter(torch.randn(self.num_node, embed_dim), requires_grad=True)
         # encoder
         self.feature_attention = feature_attention(input_dim=input_dim, output_dim=hidden_dim, kernel_size=kernel_size)
-        self.encoder = AVWDCRNN(num_node, hidden_dim, hidden_dim, cheb_k, embed_dim, num_layers)
-        #self.encoder = GPRGNN(input_dim,output_dim, hidden_dim, cheb_k,num_layers)
+        #self.encoder = AVWDCRNN(num_node, hidden_dim, hidden_dim, cheb_k, embed_dim, num_layers)
+        self.encoder = GPRGNN(num_node,input_dim,output_dim, hidden_dim, cheb_k,num_layers,embed_dim)
         self.GraphAttentionLayer = GraphAttentionLayer(hidden_dim, hidden_dim, A, dropout=0.5, alpha=0.2, concat=True)
         self.MultiHeadAttention = MultiHeadAttention(embed_size=hidden_dim, heads=heads)
         # predict
@@ -424,37 +461,19 @@ class Model(nn.Module):
 
     def forward(self, x):
         # x: (B, T, N, D)
-        batch_size = x.shape[0] #5
+        data = x
+        batch_size = x.shape[0]  # 5
+        edge_index = torch.tensor([[i, i + 1] for i in range(data.shape[2] - 1)])
         x = self.feature_attention(x)
         init_state = self.encoder.init_hidden(batch_size)
         #output, _ = self.encoder(x, init_state, self.node_embedding)  # (B, T, N, hidden_dim)
-        output, _ = self.encoder(x, init_state, self.A) 
+        # output, _ = self.encoder(data) #self.A,init_state
+        output= self.encoder(x) #self.A,init_state
         state = output[:, -1:, :, :]
         state = self.nconv(state)
         SAtt = self.GraphAttentionLayer(state)
         TAtt = self.MultiHeadAttention(output).permute(0, 2, 1, 3)
         out = SAtt + TAtt
         out = self.end_conv(out.permute(0, 3, 2, 1))  # [B, 1, N, T] -> [B, N, T]
-        out = out.permute(0, 3, 2, 1)   # [B, T, N]
-        return out
-    
-    def forward2(self, x, adj):
-    # x: (B, T, N, D)
-        batch_size = x.shape[0]
-        x = self.feature_attention(x)
-    
-    # Initialize hidden state using the adjacency matrix
-        init_state = self.encoder.init_hidden(batch_size, adj)
-
-    # Use the adjacency matrix instead of node_embedding in the encoder
-        output, _ = self.encoder(x, init_state, adj)
-
-    # Other parts of the code remain the same
-        state = output[:, -1:, :, :]
-        state = self.nconv(state)
-        SAtt = self.GraphAttentionLayer(state, adj)
-        TAtt = self.MultiHeadAttention(output).permute(0, 2, 1, 3)
-        out = SAtt + TAtt
-        out = self.end_conv(out.permute(0, 3, 2, 1))
-        out = out.permute(0, 3, 2, 1)
+        out = out.permute(0, 3, 2, 1)  # [B, T, N]
         return out
