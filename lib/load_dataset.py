cb966aef9e808b7768b7f1c2d943c7801951fecf
BeyzanurYuce <94442443+BeyzanurYuce@users.noreply.github.com>
2024-12-22 22:03:23 +0300

    Update load_dataset.py

--- lib/load_dataset.py
+++ lib/load_dataset.py
@@ -7,13 +7,19 @@ def load_st_dataset(dataset):
         data_path = os.path.join('/content/AFDGCN_Garnoldi/data/PEMS04/PEMS04.npz')
         data = np.load(data_path)['data'][:, :, :1]  # only the first dimension, traffic flow data
     elif dataset == 'PEMS08':
-        data_path = os.path.join('./data/PEMS08/PEMS08.npz')
+        data_path = os.path.join('/content/AFDGCN_Garnoldi/data/PEMS08/PEMS08.npz')
         data = np.load(data_path)['data'][:, :, :1]  # only the first dimension, traffic flow data
     elif dataset == 'PEMS03':
-        data_path = os.path.join('./data/PEMS03/PEMS03.npz')
+        data_path = os.path.join('/content/AFDGCN_Garnoldi/data/PEMS03/PEMS03.npz')
         data = np.load(data_path)['data'][:, :, :1]  # only the first dimension, traffic flow data
     elif dataset == 'PEMS07':
-        data_path = os.path.join('./data/PEMS07/PEMS07.npz')
+        data_path = os.path.join('/content/AFDGCN_Garnoldi/data/PEMS07/PEMS07.npz')
+        data = np.load(data_path)['data'][:, :, :1]  # only the first dimension, traffic flow data
+    elif dataset == 'Kcetas':
+        data_path = os.path.join('/content/AFDGCN_Garnoldi/data/Kcetas/morning_only.npz')
+        data = np.load(data_path)['data'][:, :, :1] 
+    elif dataset == 'Konya':
+        data_path = os.path.join('/content/AFDGCN_Garnoldi/data/Konya/konya_kavşak.npz')
         data = np.load(data_path)['data'][:, :, :1]  # only the first dimension, traffic flow data
     else:
         raise ValueError
